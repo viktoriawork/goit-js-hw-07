bd34103e0f86355895b676ed628906dd61cbb65e
viktoriawork <viktoriawork2@GMAIL.COM>
2021-06-16 23:14:33 +0200

    chabges

--- js/task-03.js
+++ js/task-03.js
@@ -2,6 +2,7 @@ const images = [
     {
       url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
       alt: 'White and Black Long Fur Cat',
+      
     },
     {
       url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
